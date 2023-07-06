4db18d52289ce364b660bbe847351028c9b38b20
Valerie <valeriejeptoo4@gmail.com>
2023-07-06 21:53:39 +0300

    updated html

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-let result = document.getElementById("result");
-let searchBtn = document.getElementById("search-btn");
-let url ="https://apilist.fun/api/the-cocktail-db";
-let userInp = document.getElementById("user-inp").ariaValueMax;
-
-if(userInp.length == 0) {
-    result.innerHTML = `<h3 class ="msg">The input field
-    cannot be empty</h3>`;
-    `
-}
+// Function to fetch data from API
+function fetchData(url) {
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data); // Log the fetched data
+        return data;
+      })
+      .catch(error => console.log(error));
+  }
+  
+  // Function to update card content
+  function updateCard(cocktail) {
+    document.getElementById('card-image').src = cocktail.strDrinkThumb;
+    document.getElementById('card-title').textContent = cocktail.strDrink;
+    document.getElementById('card-quote').textContent = cocktail.strAlcoholic;
+    document.getElementById('card-source').textContent = cocktail.strCategory;
+  }
+  
+  // Search form event listener
+  document.getElementById('search-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    const searchInput = document.getElementById('search-input').value;
+    const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + encodeURIComponent(searchInput);
+  
+    // Fetch data and update card content
+    fetchData(apiUrl)
+      .then(data => {
+        if (data.drinks && data.drinks.length > 0) {
+          updateCard(data.drinks[0]); // Update card with the first result
+        } else {
+          console.log('No results found.');
+        }
+      });
+  });
+    
