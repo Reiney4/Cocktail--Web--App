ba4409abe0bff6911b2a2736dd8135b04e3599ad
Valerie <valeriejeptoo4@gmail.com>
2023-07-07 16:03:17 +0300

    updated fetch in js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ fetch('https://api.npoint.io/67a99d45586a9c1302f8/drinks/')
     // Get the container element
     const cardContainer = document.getElementById('cardContainer');
 
-    // Iterate over the data and create a card for each item
-    data.forEach(cocktail => {
+    // Function to create cards for the cocktails
+    const createCard = (cocktail) => {
       // Create card elements
       const card = document.createElement('div');
       card.className = 'col';
       card.innerHTML = `
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="${cocktail.strDrinkThumb}" alt="Card Image">
+        <div class="card" style="width: 9 rem;">
+          <img id="cocktail_img" class="card-img-top" src="${cocktail.strDrinkThumb}" alt="Card Image">
           <div class="card-body">
             <h5 class="card-title">${cocktail.strDrink}</h5>
             <p class="card-quote">${cocktail.strAlcoholic}</p>
@@ -23,6 +23,35 @@ fetch('https://api.npoint.io/67a99d45586a9c1302f8/drinks/')
 
       // Append the card to the container
       cardContainer.appendChild(card);
+    };
+
+    // Iterate over the data and create cards for each cocktail
+    data.forEach(cocktail => {
+      createCard(cocktail);
+    });
+
+    // Function to filter the data based on the search input
+    const filterData = (searchTerm) => {
+      // Clear previous search results
+      cardContainer.innerHTML = '';
+
+      // Filter the data based on the search term
+      const filteredData = data.filter(cocktail => cocktail.strDrink.toLowerCase().includes(searchTerm.toLowerCase()));
+
+      // Create cards for the filtered data
+      filteredData.forEach(cocktail => {
+        createCard(cocktail);
+      });
+    };
+
+    // Handle form submission
+    const searchForm = document.getElementById('search-form');
+    const searchInput = document.getElementById('search-input');
+
+    searchForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const searchTerm = searchInput.value;
+      filterData(searchTerm);
     });
   })
   .catch(error => {
